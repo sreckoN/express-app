6cb139fec563c612f0ad462791a19f7dd15f5965
sreckon <srecko.nikolic@protonmail.com>
2023-03-29 18:33:59 +0200

    Changed the message 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Worldyy!</h1>')
+  res.send('<h1>Hello Worldyyy!</h1>')
 })
 
 const PORT = 8080
